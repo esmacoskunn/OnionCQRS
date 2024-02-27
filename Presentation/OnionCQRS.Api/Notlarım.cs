33e42b921a8ff2111a81116a10a099689a67c2f3
esmacoskunn <137655982+esmacoskunn@users.noreply.github.com>
2024-02-27 22:59:32 +0300

    dev,pro alanları

---
+++ Presentation/OnionCQRS.Api/Notlarım.cs
@@ -0,0 +1,28 @@
+﻿namespace OnionCQRS.Api
+{
+    public class Notlarım
+    {
+
+        //appsettings.Development : burası benim localimdeki msssql olur.
+        //appsettings.Production  : burası ise sunucu tarafındaki mssql olur.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
