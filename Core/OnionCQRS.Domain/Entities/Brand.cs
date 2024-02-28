aad76e77e19c3a0e926649ec1692ccb1d4f49c27
esmacoskunn <137655982+esmacoskunn@users.noreply.github.com>
2024-02-28 23:29:14 +0300

    entityler oluşturuldu

---
+++ Core/OnionCQRS.Domain/Entities/Brand.cs
@@ -0,0 +1,21 @@
+﻿using OnionCQRS.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCQRS.Domain.Entities
+{
+    public class Brand: EntityBase
+    {
+        public Brand()
+        {
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
---
+++ Core/OnionCQRS.Domain/Entities/Category.cs
@@ -0,0 +1,29 @@
+﻿using OnionCQRS.Domain.Common;
+using OnionCQRS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCQRS.Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        public Category()
+        {
+
+        }
+        public Category(int parentId, string name, int priorty)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priorty = priorty;
+        }
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priorty { get; set; }
+        public ICollection<Detail> Details { get; set; }
+
+    }
+}
